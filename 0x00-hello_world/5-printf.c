846dca9e307bd22b99ad8302aa3ce49ef10a8049
Ofiare Khoko <williamkhoko@yahoo.com>
2023-03-17 11:26:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammar, but the outcome is 
- * a piece of art," followed newline
+ * main - prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
  * Return: Alwys: Always 0.
  *
  */
