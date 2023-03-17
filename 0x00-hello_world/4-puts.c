d873d468a4d25f1963b55340b4305b14f79a760a
Ofiare Khoko <williamkhoko@yahoo.com>
2023-03-17 11:11:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by newline."
- * return 0.
+ * puzzle, followed by newline.
+ * Return: Always 0.
  */
 
 int main(void)
@@ -12,5 +12,6 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
+
 }
