401b00672e5a85ba191d33ec954673cf123cb5b9
Ofiare Khoko <williamkhoko@yahoo.com>
2023-03-18 11:49:30 -0700

    101-quote.c#

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
-/**main - prints "and that pieece of art is useful" - Dora Korpar, 2015-10-19",
+/**
+ * main - prints "and that pieece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a newline, to standard error.
  * Return: Always 1.
  *
@@ -10,7 +11,7 @@
  int main(void)
 
 {
-	write(2, "and that piece of art is useful\" -Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
